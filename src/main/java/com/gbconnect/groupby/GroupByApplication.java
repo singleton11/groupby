86194caa6babd6f051d9e25d13c727d1895e3dcd
Anton Prokhorov <betrayer11@gmail.com>
2018-02-06 23:47:04 +0700

    Initial commit

---
+++ src/main/java/com/gbconnect/groupby/GroupByApplication.java
@@ -0,0 +1,12 @@
+package com.gbconnect.groupby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupByApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupByApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gbconnect/groupby/GroupByApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gbconnect.groupby;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupByApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
