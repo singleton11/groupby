e7ca1ef81c96ea9e546e786e59ca45fe43f48da7
Anton Prokhorov <betrayer11@gmail.com>
2018-02-10 03:02:28 +0700

    Remove exposing all repositories

--- src/main/java/com/gbconnect/groupby/repositories/RoleRepository.java
+++ src/main/java/com/gbconnect/groupby/repositories/RoleRepository.java
@@ -2,9 +2,7 @@ package com.gbconnect.groupby.repositories;
 
 import com.gbconnect.groupby.domain.Role;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
     Role findByAuthority(String authority);
 }
--- src/main/java/com/gbconnect/groupby/repositories/UserRepository.java
+++ src/main/java/com/gbconnect/groupby/repositories/UserRepository.java
@@ -2,10 +2,9 @@ package com.gbconnect.groupby.repositories;
 
 import com.gbconnect.groupby.domain.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
     boolean existsByUsername(String username);
 }
