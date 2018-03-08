76a3b031ff1f4a745753336f4db1d6c9d925e4a9
Anton Prokhorov <betrayer11@gmail.com>
2018-03-09 01:41:02 +0700

    Add rest validation to repositories

--- src/main/java/com/gbconnect/groupby/persistence/UserRepository.java
+++ src/main/java/com/gbconnect/groupby/persistence/UserRepository.java
@@ -2,7 +2,6 @@ package com.gbconnect.groupby.persistence;
 
 import com.gbconnect.groupby.domain.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
